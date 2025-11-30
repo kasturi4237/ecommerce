45a0bcb5ab520ed889822e9d7d15ee85eddf5fb5
kasturi4237 <kastum7890@gmail.com>
2025-11-30 23:58:06 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
+
+  const [visible,setVisible]=useState(false)
   return (
     <div className='flex item-center justify-between py-5 font-medium'>
       <img src={assets.logo} className='w-36' alt="" />
@@ -51,12 +53,12 @@ const Navbar = () => {
       <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[-8px]'>10</p>
       
       </Link>
-      
+      <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
 
 
      </div>
-    
-    
+    {/*sidebar menu for small screen*/}
+    <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full':'w-0'} `}></div>    
     </div>
   )
 }
