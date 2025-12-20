3332c139072b1fd1b9101e7c67315570aa9e7432
kasturi4237 <kastum7890@gmail.com>
2025-12-20 18:58:34 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,10 +60,16 @@ const Navbar = () => {
     {/*sidebar menu for small screen*/}
     <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full':'w-0'} `}>
       <div className='flex flex-col text-gray-600'>
-        <div className='flex items-center gap-4 p-3 '>
+        <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 '>
           <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
           <p>Back</p>
         </div>
+        <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+        <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+        <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+        <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+        
+        
       </div>
       </div>    
     </div>
