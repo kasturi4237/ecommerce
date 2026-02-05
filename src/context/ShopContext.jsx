4981fc7fe4bf585f2e0d8a445a3aa0c03d88d549
kasturi4237 <kastum7890@gmail.com>
2026-02-05 23:02:16 +0530

    context page

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import {products} from "../assets/assets";
+import { ca } from "date-fns/locale";
 export const ShopContext = createContext();
 
 const ShopContextProvider=(props)=>{
@@ -7,11 +8,40 @@ const ShopContextProvider=(props)=>{
     const delivery_fee=10;
     const [search,setSearch]= useState('');
     const [showSearch,setShowSearch]=useState(false);
+    const [cartItems,setCartItems]=useState({});
+
+
+    const addToCart=async(itemId,size)=>{
+
+        if(!size){
+            toast  
+        }
+        let cartData= structuredClone(cartItems);
+        if(cartData[itemId]){
+            if(cartData[itemId][size]){
+                cartData[itemId][size]+=1;
+            }
+            else{
+                cartData[itemId][size]=1;
+            }
+        }
+        else{
+         cartData[itemId]={};
+         cartData[itemId][size]=1;
+        }
+        setCartItems(cartData);
+    }
+
+    useEffect(()=>{
+        console.log(cartItems);
+
+    },[cartItems])
 
 
     const value={
         products,currency,delivery_fee,
-        search,setSearch,showSearch,setShowSearch
+        search,setSearch,showSearch,setShowSearch,
+        cartItems,addToCart
     }
 
     return(
